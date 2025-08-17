ee3c91b3363835fa7848016b603a2c41cfde9e80
utkuakyuz <utku145@gmail.com>
2025-08-17 17:06:08 +0300

    chore: netlify configuration

--- SpeciesAssemlyExplorerREACT/vite.config.ts
+++ SpeciesAssemlyExplorerREACT/vite.config.ts
@@ -4,6 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]"
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": "/src",
